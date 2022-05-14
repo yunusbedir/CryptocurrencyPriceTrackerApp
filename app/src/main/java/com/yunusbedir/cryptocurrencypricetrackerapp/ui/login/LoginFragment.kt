8e100b459ee048a27cf8f3f8d0be18a20ebd5b45
ybedir <yunusbedir@outlook.com.tr>
2022-05-14 12:57:51 +0300

    implemented navigation component

---
+++ app/src/main/java/com/yunusbedir/cryptocurrencypricetrackerapp/ui/login/LoginFragment.kt
@@ -0,0 +1,24 @@
+package com.yunusbedir.cryptocurrencypricetrackerapp.ui.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.yunusbedir.cryptocurrencypricetrackerapp.databinding.FragmentLoginBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(inflater)
+        return binding.root
+    }
+
+}
